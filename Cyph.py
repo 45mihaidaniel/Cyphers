6dcf88f5789dd82bba37acc795842d66081b9ad0
45mihaidaniel <62219788+45mihaidaniel@users.noreply.github.com>
2020-11-03 19:28:31 +0200

    Add files via upload

---
+++ Cyph.py
@@ -0,0 +1,33 @@
+#This is a module that contains ciphers for text
+#Use the functions to encrypt messages
+
+#Ceaser Cipher with one shift
+# A -> Z
+# B -> A and so on...
+def CCE(text):#Encrypter
+   cipher=''
+   for char in text:
+      if not char.isalpha():
+         continue
+      char=char.upper()
+      code=ord(char)-1
+      if code<ord('A'):
+         code=ord('Z')
+      cipher+=chr(code)
+   return cipher
+#Decrypter
+def CCD(text):
+   decrypt=''
+   for char in text:
+      if not char.isalpha():
+         continue
+      char=char.upper()
+      code=ord(char)+1
+      if code>ord('Z'):
+         code=ord('A')
+      decrypt+=chr(code)
+   return decrypt
+cifru=CCE("Ana are merele")
+print(cifru)
+decifrare=CCD(cifru)
+print(decifrare)
